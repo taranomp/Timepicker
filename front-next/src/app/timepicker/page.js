7d1f6bc97a14304ac66313473a8d6b37d9b4369f
taranomp <mohamadpour.t2002@gmail.com>
2025-12-23 04:18:52 -0800

    forth commit add endpoint

--- front-next/src/app/timepicker/page.js
+++
@@ -1,7 +0,0 @@
-export default function TimePickerPage() {
-    return (
-        <div className="container-fluid px-auto">
-            <h1 className="pt-5">Time Picker Page</h1>
-        </div>
-    );
-}
--- timepicker/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class TimepickerConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'timepicker'
